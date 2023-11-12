d55806e5d38949c95379d53c93b2764c08f0b7fe
Shibo Xing <stanleyhsing26@gmail.com>
2023-11-12 16:21:04 +0800

    feat: drag 3d objects to the ray cast hit point position

--- ball_screw_linear_slide_unity3d/Assets/Scripts/drag_and_drop.cs
+++ ball_screw_linear_slide_unity3d/Assets/Scripts/drag_and_drop.cs
@@ -18,21 +18,38 @@ public class drag_and_drop : MonoBehaviour, IBeginDragHandler, IDragHandler, IEn
     }
     private Vector3 init_pos;
     private Transform new_obj;
+    private int drag_mask;
     public void OnBeginDrag(PointerEventData eventData)
     {
         var obj = gameObject.transform.Find("real_obj");
         init_pos = gameObject.transform.position;
         new_obj = Instantiate(obj.transform);
         new_obj.SetParent(null);
+        new_obj.transform.localScale = Vector3.one * 100;
+        new_obj.gameObject.layer = LayerMask.NameToLayer("on_drag");
+        //drag_mask = 1 << new_obj.gameObject.layer;
     }
+
     public void OnDrag(PointerEventData eventData)
     {
-        transform.position = eventData.position;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+        {
+            new_obj.transform.position = hit.point;
+            transform.position = init_pos;
+        }
+        else
+        {
+            transform.position = eventData.position;
+            new_obj.transform.position = Vector3.zero;
+        }
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
         transform.position = init_pos;
-        new_obj.position = eventData.position;
+        new_obj.position = Vector3.zero;
+        Destroy(new_obj.gameObject);
     }
 }
