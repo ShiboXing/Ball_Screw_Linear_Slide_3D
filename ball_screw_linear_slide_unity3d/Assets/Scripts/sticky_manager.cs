bfee7252fc290ee3f249d91b416b8bd4476cb650
Shibo Xing <shx26@pitt.edu>
2023-12-16 16:43:36 +0800

    fix: the OFFSET between sticky object's Renderer's bound and transform

--- ball_screw_linear_slide_unity3d/Assets/Scripts/sticky_manager.cs
+++ ball_screw_linear_slide_unity3d/Assets/Scripts/sticky_manager.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 [ExecuteAlways]
@@ -34,6 +35,7 @@ public sealed class sticky_manager : MonoBehaviour
             float h_perc = bds[i + 4];
             float l_perc = bds[i + 5];
 
+
             var x_base = bd.min.x + bd.size.x * x_perc;
             var y_base = bd.min.y + bd.size.y * y_perc;
             var z_base = bd.min.z + bd.size.z * z_perc;
@@ -50,10 +52,14 @@ public sealed class sticky_manager : MonoBehaviour
                 && y_base + h >= y_hit && y_hit >= y_base
                 && z_base + l >= z_hit && z_hit >= z_base)
             {
+                // RE-ADJUST for the OFFSET between the Renderer's BOUND and TRANSFORM
+                var sticky_bds = sticky_trans.gameObject.GetComponent<Renderer>().bounds;
+                var offset = sticky_trans.position - sticky_bds.center;
+
                 // reset it to the CENTER of the bound
-                var x_mid = x_base + w / 2;
-                var y_mid = y_base + h / 2;
-                var z_mid = z_base + l / 2;
+                var x_mid = x_base + offset.x + w / 2;
+                var y_mid = y_base + offset.y + h / 2;
+                var z_mid = z_base + offset.z + l / 2;
                 if (Mathf.Abs(x_mid) == float.PositiveInfinity)
                     sticky_trans.position = new Vector3(x_hit, y_mid, z_mid);
                 else if (Mathf.Abs(y_mid) == float.PositiveInfinity)
@@ -78,6 +84,8 @@ public sealed class sticky_manager : MonoBehaviour
         sticky_map = new Dictionary<string, HashSet<string>>();
         sticky_map["底板"] = new HashSet<string> { "GSX60C-H0前端座" };
         sticky_map["GSX60C-H0前端座"] = new HashSet<string> { "SGSX60C000000003-轴承端盖" };
+        sticky_map["GSX60C-H0前端座"].Add("SGSX060CM00670015-GSX60C丝杆");
+        sticky_map["SGSX060CM00670015-GSX60C丝杆"] = new HashSet<string> { "SGSX60C000000004-圆螺母" };
 
         // instantiate the bound coordinates mapping
         sticky_bounds = new Dictionary<string, Dictionary<string, List<float>>>();
@@ -93,6 +101,7 @@ public sealed class sticky_manager : MonoBehaviour
         }
         sticky_bounds["底板"]["GSX60C-H0前端座"].AddRange(new float[] { 0.74f, 0f, 0.32f, 0.07f, float.PositiveInfinity, 0.35f });
         sticky_bounds["GSX60C-H0前端座"]["SGSX60C000000003-轴承端盖"].AddRange(new float[] { -0.1f, 0.27f, 0.21f, 0.13f, 0.48f, 0.61f });
+        sticky_bounds["GSX60C-H0前端座"]["SGSX060CM00670015-GSX60C丝杆"].AddRange(new float[] { -10.64312f, 0.3883308f, 0.3909872f, 11.63333f, 0.25f, 0.2142864f });
     }
 
     // Update is called once per frame
